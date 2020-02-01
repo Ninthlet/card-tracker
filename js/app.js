abcf8adda442614f1fc1a0af8fb74fbb2453d842
Ninthlet <36407812+Ninthlet@users.noreply.github.com>
2020-02-01 15:27:18 +0100

    Structure

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var app = angular.module("card-container", []);
+app.controller("card-controller", function ($scope) {
+    $scope.cards = [
+        {
+            "title": "Card title 1",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "01/02/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        },
+        {
+            "title": "Card title 2",
+            "notes": "Lorem ipsum dolores",
+            "addDate": "31/01/2020"
+        }
+    ]
+});
